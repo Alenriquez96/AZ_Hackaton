2a9c635ec1f2c3c720f28d703446ec11d6f8cbb7
Alberto Enriquez Enriquez <aenriquezenriquez@deloitte.es>
2024-03-15 10:41:44 +0100

    add suspense to context

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -3,12 +3,15 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { UserProvider } from "@/app/context/UserContext";
+import { Suspense } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="light">
-        <UserProvider>{children}</UserProvider>
+        <Suspense>
+          <UserProvider>{children}</UserProvider>
+        </Suspense>
       </NextThemesProvider>
     </NextUIProvider>
   );
